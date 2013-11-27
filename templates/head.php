7b892ae505346c4df9d8abd42d57d6fed7141c82
Kiko Doran <kiko.doran@gmail.com>
2013-11-27 17:45:48 -0600

    Updated head.php with spring-theme content

--- templates/head.php
+++ templates/head.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php wp_title('|', true, 'right'); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <meta name="description" content="<?php wp_title(); echo ' | '; bloginfo( 'description' ); ?>" />
+    <meta charset="utf-8">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
 
-  <?php wp_head(); ?>
+    <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
 </head>
