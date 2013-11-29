55d8987dacf254c222eeafe37facafd163db8c74
Dan Beil <dan.beil@blueearth.net>
2013-11-29 16:46:12 -0600

    removing theme activation functions

--- functions.php
+++ functions.php
@@ -7,7 +7,6 @@ require_once locate_template('/lib/init.php');            // kdInitial theme set
 require_once locate_template('/lib/wrapper.php');         // Theme wrapper class
 require_once locate_template('/lib/sidebar.php');         // Sidebar class
 require_once locate_template('/lib/config.php');          // Configuration
-require_once locate_template('/lib/activation.php');      // Theme activation
 require_once locate_template('/lib/titles.php');          // kdPage titles
 require_once locate_template('/lib/cleanup.php');         // Cleanup
 require_once locate_template('/lib/nav.php');             // Custom nav modifications
@@ -17,4 +16,3 @@ require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/custom.php');          // Custom functions
-
