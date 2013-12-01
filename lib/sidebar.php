e809c2945db62f9664174163622bc5275a792777
Kiko Doran <kiko.doran@gmail.com>
2013-12-01 14:15:38 -0600

    Removed roots link from sidebar comment

--- lib/sidebar.php
+++ lib/sidebar.php
@@ -4,8 +4,6 @@
  *
  * If any of the is_* conditional tags or is_page_template(template_file) checks return true, the sidebar will NOT be displayed.
  *
- * @link http://roots.io/the-roots-sidebar/
- *
  * @param array list of conditional tags (http://codex.wordpress.org/Conditional_Tags)
  * @param array list of page templates. These will be checked via is_page_template()
  *
